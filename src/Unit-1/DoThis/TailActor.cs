4a24c22059bc1728900f5db4037780b236a9bc2c
Peter Carroll <pete.carroll@iress.com>
2022-01-05 17:26:08 +0000

    Unit 1 Lesson 6

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -48,16 +48,19 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
-            _observer = new FileObserver(Self, Path.GetFullPath(filePath));
+        protected override void PreStart()
+        {
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
             _observer.Start();
 
             _fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
@@ -88,6 +91,15 @@ namespace WinTail
                 var ir = message as InitialRead;
                 _reporterActor.Tell(ir.Text);
             }
+        }
+
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            base.PostStop();
         }
     }
 }
